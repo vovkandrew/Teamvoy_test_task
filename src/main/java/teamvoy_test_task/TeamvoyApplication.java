293e56eba549304930b6186c0b44ce22a18ef1b0
VovkAA <vovkandrew@gmail.com>
2020-07-27 17:04:42 +0300

    A view additional files added

---
+++ src/main/java/teamvoy_test_task/TeamvoyApplication.java
@@ -0,0 +1,13 @@
+package teamvoy_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamvoyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamvoyApplication.class, args);
+    }
+
+}
